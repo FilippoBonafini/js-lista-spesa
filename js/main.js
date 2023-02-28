82a674bf9f4c5a5fb4ab8af1af749fca55e29905
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-02-28 19:24:12 +0100

    Programma finito, inizio modifica grafica

--- js/main.js
+++ js/main.js
@@ -24,17 +24,18 @@ const listDoWhileArray = [
 // INDICHIAMO L'INPUT PER AGGIUNGERE UN PRODOTTO 
 const inputFor = document.getElementById('listForInput');
 const inputWhile = document.getElementById('listWhileInput');
-const inputDowhile = document.getElementById('listDoWhileInput');
+const inputDoWhile = document.getElementById('listDoWhileInput');
 
 // INDICHIAMO IL BOTTONE PER AGGIUNGERE UN PRODOTTO 
 const buttonFor = document.getElementById('listForButton');
 const buttonWhile = document.getElementById('listWhileButton');
-const buttonDowhile = document.getElementById('listDoWhileButton');
+const buttonDoWhile = document.getElementById('listDoWhileButton');
 
 // INDICHIAMO LE UL VUOTE
 const listFor = document.getElementById('listFor');
 const listWhile = document.getElementById('listWhile');
 const listDoWhile = document.getElementById('listDoWhile');
+
 
 // PRIMA LISTA : FOR 
 for (let iFor = 0; iFor<listForArray.length; iFor++){
@@ -42,6 +43,15 @@ for (let iFor = 0; iFor<listForArray.length; iFor++){
     listFor.append(listElement);
     listElement.innerHTML = (listForArray[iFor]);
 }
+// INPUT E BOTTONI DELLA LISTA WHILE 
+buttonFor.addEventListener('click',
+    function(){
+        listForArray.push(inputFor).value;
+        const listElement = document.createElement('li');
+        listFor.append(listElement);
+        listElement.innerHTML = (inputFor).value;
+    }
+)
 
 // SECONDA LISTA : WHILE 
 let iWhile = 0;
@@ -51,6 +61,15 @@ while (iWhile < listWhileArray.length){
     listElement.innerHTML = (listWhileArray[iWhile]);
     iWhile++;
 }
+// INPUT E BOTTONI DELLA LISTA FOR 
+buttonWhile.addEventListener('click',
+    function(){
+        listWhileArray.push(inputWhile).value;
+        const listElement = document.createElement('li');
+        listWhile.append(listElement);
+        listElement.innerHTML = (inputWhile).value;
+    }
+)
 
 // TERZA LISTA: DO WHILE 
 let iDoWhile = 0;
@@ -60,3 +79,12 @@ do {
     listElement.innerHTML = (listDoWhileArray[iDoWhile]);
     iDoWhile++;
 } while ( iDoWhile < listDoWhileArray.length);
+// INPUT E BOTTONI DELLA LISTA WHILE 
+buttonDoWhile.addEventListener('click',
+    function(){
+        listDoWhileArray.push(inputDoWhile).value;
+        const listElement = document.createElement('li');
+        listDoWhile.append(listElement);
+        listElement.innerHTML = (inputDoWhile).value;
+    }
+)
